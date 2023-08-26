a6a7070c4e78da5490fc5f4e1e9a1b08a80b8771
Alex <giraldojhong4@gmail.com>
2023-08-26 18:02:33 -0500

    Chat funcional

---
+++ LIN.Chat/Client/Pages/Login.razor.cs
@@ -0,0 +1,45 @@
+﻿namespace LIN.Chat.Client.Pages;
+
+
+public partial class Login
+{
+
+    /// <summary>
+    /// Obtiene si se esta log con una llave de acceso
+    /// </summary>
+    private bool IsWithKey { get; set; } = false;
+
+
+    /// <summary>
+    /// Usuario
+    /// </summary>
+    private string User { get; set; } = "";
+
+
+    /// <summary>
+    /// Contraseña
+    /// </summary>
+    private string Password { get; set; } = "";
+
+
+    /// <summary>
+    /// Mensaje que se muestra al cargar
+    /// </summary>
+    private string LogMessage { get; set; } = "Iniciando Sesión";
+
+
+    /// <summary>
+    /// Mensaje de error
+    /// </summary>
+    private string ErrorMessage { get; set; } = "";
+
+
+
+
+    bool cancelShow = false;
+
+    private bool isLogin = false;
+
+
+
+}
