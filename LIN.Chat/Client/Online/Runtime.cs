66022bae3e11c405c9ed65245571d89a45c047de
Alex <giraldojhong4@gmail.com>
2023-08-26 13:29:07 -0500

    LIN CHAT 1

---
+++ LIN.Chat/Client/Online/Runtime.cs
@@ -0,0 +1,51 @@
+﻿
+namespace LIN.Console.Client.Online;
+
+
+/// <summary>
+/// Runtime de SILF.Script para LIN Cloud Console
+/// </summary>
+internal class Scripts
+{
+
+    /// <summary>
+    /// Lista de elementos
+    /// </summary>
+    public static Dictionary<string, Action<string>> Actions { get; set; } = new Dictionary<string, Action<string>>();
+
+
+
+    /// <summary>
+    /// Construye las funciones
+    /// </summary>
+    public static void Build()
+    {
+
+        // Actualiza los proyectos
+        Actions.Add("cl.UpdateProjects", async (param) =>
+        {
+           
+
+        });
+
+
+        // Abre modal de contacto
+        Actions.Add("openCt", async (param) =>
+        {
+            var id = int.Parse(param);
+            //var modelo =  await LIN.Access.Auth.Controllers.Contact.Read(id);
+            //ContactModal.Modelo = modelo.Model;
+            //await App.JS.InvokeVoidAsync("ShowModal", $"contact-modal-A12", "contact-close-btn-A12");
+            //ContactModal.Context.Render();
+        });
+
+
+        // Cierra la sesión
+        Actions.Add("disconnect", (param) =>
+        {
+        });
+
+
+    }
+
+}
---
+++ LIN.Chat/Client/Program.cs
@@ -0,0 +1,29 @@
+global using LIN.Types.Auth.Enumerations;
+global using LIN.Types.Auth.Models;
+global using LIN.Types.Enumerations;
+global using LIN.Types.Responses;
+global using Microsoft.JSInterop;
+using Blazored.LocalStorage;
+using LIN.Console.Client;
+using LIN.Console.Client.Online;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<LIN.Chat.Client.App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazoredLocalStorage(config =>
+      config.JsonSerializerOptions.WriteIndented = true);
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+LIN.Chat.Client.App.MyAddress = builder.HostEnvironment.BaseAddress;
+
+Scripts.Build();
+
+StaticHub.Key = LIN.Modules.KeyGen.Generate(20, "dv.");
+
+await builder.Build().RunAsync();
