47e7c9cfa024b10d022853479d4bb423aa9a21ce
Alex <giraldojhong4@gmail.com>
2023-08-29 17:11:22 -0500

    Scroll

---
+++ LIN.Chat/Client/wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿
+
+scrollToBottom = function (elementId) {
+    let element = document.getElementById(elementId);
+    if (element) {
+        let h = element.scrollHeight;
+        element.scroll({
+            top: h,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+};
