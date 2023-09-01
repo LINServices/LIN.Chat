84dde95c050452e09a63311f27afa3c5e7729265
Alex <giraldojhong4@gmail.com>
2023-09-01 09:41:27 -0500

    Separacion de logica de los componentes

---
+++ LIN.Chat/Client/Pages/Chat.razor.cs
@@ -0,0 +1,62 @@
+﻿namespace LIN.Chat.Client.Pages;
+
+
+public partial class Chat
+{
+
+
+    /// <summary>
+    /// Lista de conversaciones
+    /// </summary>
+    private readonly List<MemberChatModel> ConversaciónModels = new();
+
+
+
+    /// <summary>
+    /// Hub de conexión Realtime
+    /// </summary>
+    private LIN.Access.Communication.Hubs.ChatHub? ActualHub { get; set; }
+
+
+
+    /// <summary>
+    /// Mi perfil
+    /// </summary>
+    private MemberChatModel? Member { get; set; }
+
+
+
+    /// <summary>
+    /// Lista de Chats abiertos
+    /// </summary>
+    private readonly Dictionary<int, (Access.Communication.Hubs.ChatHub, MemberChatModel, A)> Chats = new();
+
+
+
+    /// <summary>
+    /// Pagina actual de Chat
+    /// </summary>
+    private Shared.ChatSection? ChatPage { get; set; }
+
+
+
+    /// <summary>
+    /// Cuando la pagina se inicia
+    /// </summary>
+    protected override void OnInitialized()
+    {
+
+        // Valida el login
+        if (!LIN.Access.Communication.Session.IsOpen)
+        {
+            nav.NavigateTo("/login");
+            return;
+        }
+
+        base.OnInitialized();
+    }
+
+
+
+
+}
