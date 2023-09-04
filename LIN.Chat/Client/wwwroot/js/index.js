af5d71c44f79735edca1f84ee25702be2ce6db9a
Alex <giraldojhong4@gmail.com>
2023-09-04 13:35:18 -0500

    Caracteristica ubicacion eliminada

--- LIN.Chat/Client/wwwroot/js/index.js
+++ LIN.Chat/Client/wwwroot/js/index.js
@@ -15,32 +15,3 @@ scrollToBottom = function (elementId) {
 
 
 
-
-
-
-async function getLocationPermission (dotnetReference) {
-        if ("permissions" in navigator) {
-            await navigator.permissions.query({ name: "geolocation" }).then(function (result) {
-                if (result.state === "granted" || result.state === "prompt") {
-                    dotnetReference.invokeMethodAsync("LocationPermissionGranted");
-                } else if (result.state === "denied") {
-                    dotnetReference.invokeMethodAsync("LocationPermissionDenied");
-                }
-            });
-        } else {
-            dotnetReference.invokeMethodAsync("LocationPermissionNotSupported");
-        }
-    }
-
-
-function getLocation(dotnetReference)
-{
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                console.log(position);
-                dotnetReference.invokeMethodAsync("UpdateLocation", position.coords.latitude, position.coords.longitude);
-            });
-        } else {
-            dotnetReference.invokeMethodAsync("LocationError", "Geolocation is not available in this browser.");
-        }
-    }
