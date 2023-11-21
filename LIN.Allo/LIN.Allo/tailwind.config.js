a2cdaf8f6328b391a1db1898d6fb312bfbf08bec
Alex <giraldojhong4@gmail.com>
2023-11-21 00:20:47 -0500

    Emoji Popup

--- LIN.Allo/LIN.Allo/tailwind.config.js
+++ LIN.Allo/LIN.Allo/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         extend: {
             width: {
                 'one-third': '33.333333%',
+                'ring-1.5':"box-shadow: var(--tw-ring-inset) 0 0 0 calc(1.5px + var(--tw-ring-offset-width)) var(--tw-ring-color);"
             }
         },
     },
