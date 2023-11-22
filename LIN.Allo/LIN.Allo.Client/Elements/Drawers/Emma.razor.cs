56966203e2d8c5e2376ca2525ec439b1a0fd1d93
Alex <giraldojhong4@gmail.com>
2023-11-21 21:24:17 -0500

    Mejoras de Emma.razor

---
+++ LIN.Allo/LIN.Allo.Client/Elements/Drawers/Emma.razor.cs
@@ -0,0 +1,111 @@
+﻿namespace LIN.Allo.Client.Elements.Drawers;
+
+
+public partial class Emma
+{
+
+    /// <summary>
+    /// Id único del elemento.
+    /// </summary>
+    private string UniqueId { get; init; } = Guid.NewGuid().ToString();
+
+
+
+    /// <summary>
+    /// Actual estado.
+    /// </summary>
+    private State ActualState { get; set; } = State.Witting;
+
+
+
+    /// <summary>
+    /// Lista de estados.
+    /// </summary>
+    private enum State
+    {
+        Witting,
+        Responding
+    }
+
+
+
+    /// <summary>
+    /// Entrada del usuario.
+    /// </summary>
+    private string Prompt { get; set; } = string.Empty;
+
+
+
+    /// <summary>
+    /// Respuesta de Emma.
+    /// </summary>
+    private string EmmaResponse { get; set; } = string.Empty;
+
+
+
+
+
+
+
+    /// <summary>
+    /// Abre el elemento.
+    /// </summary>
+    public async void Show()
+    {
+        await js.InvokeAsync<object>("ShowDrawer", $"drawerEmma-{UniqueId}", $"close-drawerEmma-{UniqueId}");
+        StateHasChanged();
+    }
+
+
+
+    /// <summary>
+    /// Enviar a Emma.
+    /// </summary>
+    private async void ToEmma()
+    {
+
+        // Cambia el estado.
+        ActualState = State.Responding;
+        StateHasChanged();
+
+        // Respuesta.
+        var response = await Access.Communication.Controllers.Messages.ToEmma(Prompt, Access.Communication.Session.Instance.Token);
+
+        // Cambia el estado.
+        ActualState = State.Witting;
+
+        // Es un comando.
+        if (response.Model.Content.StartsWith("#"))
+        {
+            var app = new SILF.Script.App(response.Model.Content.Remove(0, 1));
+            //app.AddDefaultFunctions(Online.Scripts.Actions);
+            app.Run();
+            EmmaResponse = "Perfecto";
+            StateHasChanged();
+            return;
+        }
+
+        // Establece la respuesta de Emma.
+        EmmaResponse = response.Model.Content;
+        StateHasChanged();
+
+        // Hablar
+        await js.InvokeVoidAsync("Speech", response.Model.Content);
+    }
+
+
+
+    /// <summary>
+    /// Invocable.
+    /// </summary>
+    [JSInvokable("OnEmma")]
+    public void OnEmma(string value)
+    {
+        Prompt = value;
+        StateHasChanged();
+        ToEmma();
+    }
+
+
+
+}
--- LIN.Allo/LIN.Allo/Program.cs
+++ LIN.Allo/LIN.Allo/Program.cs
@@ -16,7 +16,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
