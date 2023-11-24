8f4c6418a55746d00a2e22b7eff1cbecad8670d7
Alex <giraldojhong4@gmail.com>
2023-11-24 16:04:17 -0500

    Solucion de error: Confirmacion de mensajes UI

--- LIN.Allo/LIN.Allo/tailwind.config.js
+++ LIN.Allo/LIN.Allo/tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
     content: ["../**/*{html,razor,js,cs}"],
     theme: {
         extend: {
-            width: {
-                'one-third': '33.333333%',
-                'ring-1.5':"box-shadow: var(--tw-ring-inset) 0 0 0 calc(1.5px + var(--tw-ring-offset-width)) var(--tw-ring-color);"
-            }
+           
         },
     },
     plugins: [],
