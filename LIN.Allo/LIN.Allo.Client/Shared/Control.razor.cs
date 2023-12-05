1f0198c978807c28e807f2ed8f8191a2a7c3c0bc
Alex <giraldojhong4@gmail.com>
2023-12-04 21:34:22 -0500

    Optimizacion

---
+++ LIN.Allo/LIN.Allo.Client/Shared/Control.razor.cs
@@ -0,0 +1,70 @@
+﻿namespace LIN.Allo.Client.Shared;
+
+
+public partial class Control
+{
+
+    /// <summary>
+    /// Evento al darle click.
+    /// </summary>
+    [Parameter]
+    public Action<MemberChatModel> OnClick { get; set; } = (e) => { };
+
+
+
+    /// <summary>
+    /// Modelo.
+    /// </summary>
+    [Parameter]
+    public MemberChatModel Member { get; set; }
+
+
+
+    /// <summary>
+    /// El control esta seleccionado.
+    /// </summary>
+    public bool IsSelect { get; set; } = false;
+
+
+
+    /// <summary>
+    /// Tiene nuevos mensajes.
+    /// </summary>
+    public bool IsNew = false;
+
+  
+
+
+    /// <summary>
+    /// Renderizar.
+    /// </summary>
+    public void Render()
+    {
+        StateHasChanged();
+    }
+
+
+
+    /// <summary>
+    /// Seleccionar.
+    /// </summary>
+    public void Select()
+    {
+        IsSelect = true;
+        Render();
+    }
+
+
+
+    /// <summary>
+    /// Deseleccionar.
+    /// </summary>
+    public void Unselect()
+    {
+        IsSelect = false;
+        Render();
+    }
+
+
+
+}
