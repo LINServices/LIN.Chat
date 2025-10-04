9d4b6f25b85c115cc8c6f25c7fb93f74fb6778f7
Alex Giraldo <giraldojhong4@gmail.com>
2025-10-04 12:21:37 -0500

    Refactorización de CallSection y mejoras en la UI
    Se ha refactorizado el componente `CallSection` separando la lógica
    del diseño al mover el bloque `@code` a un archivo parcial
    `CallSection.razor.cs`. Esto mejora la organización y el mantenimiento
    del código.
    
    En `CallSection.razor`:
    - Se actualizó la inyección de dependencias para mayor claridad.
    - Se añadió un encabezado opcional con título y hora dinámica.
    - Se reorganizó el botón de colgar para mayor claridad.
    - Se reubicó el componente `<DevicesDrawer>` para mejorar la estructura.
    
    En `CallSection.razor.cs`:
    - Se añadieron propiedades y métodos para manejar el estado del
      micrófono, cámara, y compartir pantalla.
    - Se implementó la inicialización de WebRTC en `OnAfterRenderAsync`.
    - Se añadieron métodos para colgar llamadas y continuar en otros
      dispositivos.
    
    Estos cambios mejoran la claridad, funcionalidad y mantenibilidad del
    código, además de optimizar la experiencia de usuario.

---
+++ LIN.Allo/LIN.Allo.Client/Pages/Sections/CallSection.razor.cs
@@ -0,0 +1,133 @@
+﻿namespace LIN.Allo.Client.Pages.Sections;
+
+public partial class CallSection
+{
+    /// <summary>
+    /// Id de la llamada.
+    /// </summary>
+    [Parameter]
+    public string RoomId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Cajon share.
+    /// </summary>
+    private DevicesDrawer? devicesDrawer;
+
+    /// <summary>
+    /// Estado del micrófono.
+    /// </summary>
+    private bool MicroState { get; set; } = true;
+
+    /// <summary>
+    /// Estado de la cámara.
+    /// </summary>
+    private bool CamState { get; set; } = true;
+
+    /// <summary>
+    /// Soporta compartir campaña.
+    /// </summary>
+    private bool SupportShareScreen = false;
+
+    /// <summary>
+    /// Obtener o establecer si el dispositivo esta en una llamada.
+    /// </summary>
+    public static bool IsThisDeviceOnCall { get; set; } = false;
+
+    /// <summary>
+    /// Obtener o establecer si el dispositivo esta compartiendo pantalla.
+    /// </summary>
+    public bool IsSharingScreen { get; set; } = false;
+
+    /// <summary>
+    /// Después de renderizar.
+    /// </summary>
+    protected override async Task OnAfterRenderAsync(bool first)
+    {
+        if (first)
+        {
+            SupportShareScreen = await JSRuntime.InvokeAsync<bool>("supportShareScreen");
+            await JSRuntime.InvokeVoidAsync("webrtc.init", "https://api.linplatform.com/Communication/hub/calls", Access.Communication.Session.Instance.Account.Name);
+            await JSRuntime.InvokeVoidAsync("webrtc.join", int.Parse(RoomId), LIN.Access.Communication.Session.Instance.Token); // asegura que RoomId está seteado
+            IsThisDeviceOnCall = true;
+            StateHasChanged();
+        }
+    }
+
+    /// <summary>
+    /// Compartir / dejar de compartir pantalla.
+    /// </summary>
+    private async void ToggleShare()
+    {
+        if (IsSharingScreen)
+        {
+            await StopShare();
+            return;
+        }
+        await ShareScreen();
+    }
+
+    /// <summary>
+    /// Compartir pantalla.
+    /// </summary>
+    private async Task ShareScreen()
+    {
+        IsSharingScreen = await JSRuntime.InvokeAsync<bool>("webrtc.startScreenShare");
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// Dejar de compartir pantalla.
+    /// </summary>
+    private async Task StopShare()
+    {
+        IsSharingScreen = await JSRuntime.InvokeAsync<bool>("webrtc.stopScreenShare");
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// Activar / desactivar el microfono.
+    /// </summary>
+    private async Task ToggleMute()
+    {
+        var state = await JSRuntime.InvokeAsync<bool>("webrtc.toggleMute");
+        MicroState = !state;
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// Activar / desactivar la camara.
+    /// </summary>
+    private async Task ToggleCamera()
+    {
+        var state = await JSRuntime.InvokeAsync<bool>("webrtc.toggleCamera");
+        CamState = !state;
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// Colgar una llamada.
+    /// </summary>
+    private async void Hang()
+    {
+        await JSRuntime.InvokeVoidAsync("webrtc.hangup");
+        NavigationContext.NavigateTo("/home");
+        IsThisDeviceOnCall = false;
+    }
+
+    /// <summary>
+    /// Continuar en otro dispositivo.
+    /// </summary>
+    private void ContinueOn()
+    {
+        devicesDrawer?.Show();
+    }
+
+    /// <summary>
+    /// Enviar evento para pasar la llamada.
+    /// </summary>
+    private void ContinueOn(DeviceOnAccountModel device)
+    {
+        Hang();
+        HubClient.SendCommand(device.ConnectionId, RoomId);
+    }
+}
